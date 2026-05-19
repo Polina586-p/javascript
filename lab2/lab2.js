4dfd8592e57413caebbae9f880a0237ba3124483
Polina586-p <polinazuravleva586@gmail.com>
2026-05-19 14:43:25 +0300

    update lab2

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -20,3 +20,26 @@ function factorial(n){
     return BigInt(n)*factorial(n-1);
     
 }
+
+// 2.5 функция fib(n), которая возвращает n-е число Фибоначчи.
+function fib(n){ 
+    if(n==0) return 0n;
+
+    
+}
+
+// 2.6 функция compare(), которая принимает целочисленное значение x и возвращает анонимную функцию.
+function compare(n){ 
+
+}
+
+// 2.7 функция sum(), которая возвращает сумму всех своих аргументов.
+function sum(n){ 
+
+}
+
+// 2.8 функция addBlackSpot(), которая принимает на вход объект и возвращает этот объект с добавленным к нему символьным свойством blackSpot=true, которое бёрется из глобального реестра символов.
+function addBlackSpot(obj){ 
+    obj[Symbol.for("blackSpot")] = true;
+    return obj;
+}
