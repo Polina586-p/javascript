48e65334f0cc73b8efaf9dc1513fb58cf7763d67
Polina586-p <polinazuravleva586@gmail.com>
2026-05-19 14:22:35 +0300

    update lab2

---
+++ lab2/lab2.js
@@ -0,0 +1,26 @@
+
+// 2.1 функция pow(x,n), которая возвращает x в степени n (n - целое число). 
+function pow(x,n){
+    return x**n;
+}
+
+// 2.2 функция sumTo(n), которая для данного натурального n вычисляет сумму чисел от 1 до n включительно.
+function sumTo(n){
+    for(i=0; i<n+1; i++){
+        sum+=i;
+    }
+    return sum;
+}
+
+// 2.3 функция-предикат isLeapYear(year), которая проверят год year на високосность.
+function isLeapYear(year){
+    if(year%400==0||year%4==0&&year%100!=0) return True;
+}
+
+// 2.4 функция factorial(n), которая возвращает факториал числа n!, используя рекурсивный вызов n!= n*(n-1)!. 
+function factorial(n){ 
+    for(i=1; i<n+1; i++){
+        fac;
+    }
+    return sum; 
+}
