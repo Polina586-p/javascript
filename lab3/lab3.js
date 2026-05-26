ddd40e6b2764833682d3bbe3a9f75ff0faaa54fd
Polina586-p <polinazuravleva586@gmail.com>
2026-05-26 14:29:45 +0300

    update lab 3

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -1,7 +1,7 @@
 // 3.1 Функция getDecimal(num), которая возвращает дробную часть числа num.
 
 export function getDecimal(num){
-    return num%1;
+    return+(num>=0?num%1:1+(num%1)).toFixed(2);
 }
 
 // 3.2  Функция normalizeUrl(url), которая выполняет так называемую нормализацию данных.
@@ -13,25 +13,32 @@ export function normalizeUrl(url){
    if(url.startsWith('https://')){
     return url;
    }
-   return 'http://' + url;
+   return 'https://' + url;
 }
 
 // 3.3 Функция checkSpam(str), которая возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false. 
 
 export function checkSpam(str){
-
+    const lowerStr = str.toLowerCase();
+    return lowerStr.includes('viagra')||lowerStr.includes('xxx');
 }
 
 // 3.4 Функция truncate(str, maxlength), которая проверяет длину строки str, и если она превосходит maxlength – заменяет конец str на символ многоточие "…" (номер в Юникоде U+2026), так чтобы ее длина стала равна maxlength. 
 
 export function truncate(str, maxlength){
-    
+    if(str.length>maxlength)
+        return str.slice(0,maxlength-1)+ '…';
+    return str;
 }
 
 // 3.5 Функция camelize(str), которая преобразуйте строку вида 'var-test-text' в 'varTestText'.
 
 export function camelize(str){
-
+    return str.split('-').map((word,index)=>{
+        if(index===0)
+            return word;
+        return word?word[0].toUpperCase()+word.slice(1):'';
+    }).join('');
 }
 
 // 3.6 Функция fibs(n), которая для натурального n возвращает массив, заполненный числами Фибоначчи до n-го (не включая его).
@@ -49,11 +56,11 @@ export function fibs(n){
 // 3.7 Функция arrReverseSorted(arr), которая принимает неупорядоченный массив чисел arr и возвращает массив из тех же элементов, но отсортированный по убыванию.
 
 export function arrReverseSorted(arr){
-
+    return arr.slice().sort((a,b)=>b-a);
 }
 
 // 3.8 Функция unique(arr), которая вернёт массив уникальных, не повторяющихся значений массива arr. 
 
 export function unique(arr){
-
+    return [...new Set(arr)];
 }
