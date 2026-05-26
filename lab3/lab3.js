2d04e0c37ad415a47a47337b015a348025143231
Polina586-p <polinazuravleva586@gmail.com>
2026-05-26 12:44:57 +0300

    update lab 3

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -3,3 +3,15 @@
 function getDecimal(num){
     return num%1;
 }
+
+// 3.2  Функция normalizeUrl(url), которая выполняет так называемую нормализацию данных.
+
+function normalizeUrl(url){
+   if(url.startsWith('http://')){
+    return 'https://' + url.slice(7);
+   }
+   if(url.startsWith('https://')){
+    return url;
+   }
+   return 'http://' + url;
+}
